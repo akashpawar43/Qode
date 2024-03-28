c5be0c3e417123103e3645aa2f4adb546899012a
Akash Pawar <akashgp1643pawar+git@gmail.com>
2024-03-28 11:14:12 +0530

    alert improve

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Brands from './components/Brands'
-import Tools from './components/Tools'
-import Trust from './components/Trust'
-import Testimonials from './components/Testimonials'
-import Footer from './components/Footer'
-import Price from './components/Price'
-import Projects from './components/Projects'
-import Trial from './components/Trial'
-import NewsLetter from './components/NewsLetter'
-import Alert from './components/Alert'
+import React, { useEffect } from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Brands from './components/Brands';
+import Tools from './components/Tools';
+import Trust from './components/Trust';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
+import Price from './components/Price';
+import Projects from './components/Projects';
+import Trial from './components/Trial';
+import NewsLetter from './components/NewsLetter';
+import Alert from './components/Alert';
+import { useRecoilState } from 'recoil';
+import { alertAtom } from './store/atom/alert';
 
 export default function App() {
+  const [alert, setAlert] = useRecoilState(alertAtom);
+
+  useEffect(() => {
+    if (alert) {
+      setTimeout(() => {
+        setAlert(false);
+      }, 3000);
+    }
+  }, [alert]);
   return (
     <>
       <Alert />
---
+++ src/store/atom/alert.js
@@ -0,0 +1,7 @@
+import { atom } from "recoil";
+
+
+export const alertAtom = atom ({
+    key: "alertAtom",
+    default: false
+});
